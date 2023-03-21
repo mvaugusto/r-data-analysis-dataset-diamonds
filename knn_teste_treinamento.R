f1642ede68a689a1d2d279c1719c49c52b97152b
Marcus Vinícius Augusto <35598682+mvaugusto@users.noreply.github.com>
2023-03-20 21:59:33 -0300

    Rename knn_1.R to knn_teste_treinamento.R

--- knn_teste_treinamento.R
+++ knn_teste_treinamento.R
@@ -13,15 +13,15 @@ col_classe <- 11
 col_dados
 col_classe
 
-L <- sample(1:nrow(dataset1),round(nrow(dataset1)/3)) #gera lista aleatória com 1/3
+L <- sample(1:nrow(dataset1),round(nrow(dataset1)/3)) #gera lista aleatÃ³ria com 1/3
 teste       <- dataset1[L,col_dados]  # L 1/3 da base
 treinamento <- dataset1[-L,col_dados] #complemento de L, 2/3 da base
 
 head(teste)       #ver se preencheu certo 
 tail(treinamento) #ver se preencheu certo 
 
-dataset1[-L,col_classe]         #rótulo no complementar 2/3
-factor(dataset1[-L,col_classe]) # variavel categórica
+dataset1[-L,col_classe]         #rÃ³tulo no complementar 2/3
+factor(dataset1[-L,col_classe]) # variavel categÃ³rica
 
 acuracia = c(1:100)*0
 cl <- factor(dataset1[-L,col_classe])
@@ -30,7 +30,7 @@ for (i in 1:100)
 {
   fit <- knn(treinamento, teste, cl, k=12 )
 
-# matriz de confusão
+# matriz de confusÃ£o
 # fit[1:length(L)]
 #                        table(fit[1:length(L)],factor(dataset1[L,col_classe]))
   matriz_de_confusao <- table(fit[1:length(L)],factor(dataset1[L,col_classe]))
@@ -38,8 +38,8 @@ for (i in 1:100)
 }  
 
 
-print(acuracia) #tá estranho, todos iguais
+print(acuracia) #tÃ¡ estranho, todos iguais
 
-#Accuracy, precisão, exatidão
+#Accuracy, precisÃ£o, exatidÃ£o
 print(matriz_de_confusao)
 cat('Acuracia : ', sum(diag(matriz_de_confusao)) / sum(matriz_de_confusao)*100,'%')
